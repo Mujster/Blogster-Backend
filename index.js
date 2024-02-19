a7b2caa603cc09e8bcbb8707b96da32a0c6ca888
mujtaba ahmad <mmujtabach1234@gmail.com>
2024-02-20 02:11:43 +0500

    modify index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("G-Lock API & Server Running...");
 });
 
 app.listen(3001, () => {
